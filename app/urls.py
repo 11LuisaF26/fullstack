fe3e7c331435b6c7d9869d3e54a275a3d9d002da
Juan David Parada Rodriguez <juand.paradar@utadeo.edu.co>
2020-10-09 17:34:24 -0500

    version diseño

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,15 @@ from app import views
 urlpatterns = [    
     path('', views.index, name='home'),
     path('lugar', views.lugar, name= 'lugar'),
-    path('registrarLugar', views.registrarLugar, name= 'registrarLugar'),
     path('pareja', views.pareja, name= 'pareja'),
+    path('pelea', views.pelea, name= 'pelea'),
+    path('caballero', views.caballero, name= 'caballero'),
+    path('registrarLugar', views.registrarLugar, name= 'registrarLugar'),
+    path('registrarPecado', views.registrarPecado, name= 'registrarPecado'),
+    path('registrarPelea', views.registrarPelea, name= 'registrarPelea'),
+    path('registrarCaballero', views.registrarCaballero, name= 'registrarCaballero'),
     path('registrarPareja', views.registrarPareja, name= 'registrarPareja')
+     
+    
 ]
+
--- app/views.py
+++ app/views.py
@@ -9,9 +9,20 @@ def lugar(request):
     return render(request,"Lugares.html")
 def pareja(request):
     return render(request,"parejas.html")
+def pelea(request):
+    return render(request,"peleas.html")
+def caballero(request):
+    return render(request,"caballero.html")
 def registrarLugar(request):
     return render(request,"Registro_Lugares.html")
+def registrarPecado(request):
+    return render(request,"Registro_Pecados.html")
+def registrarPelea(request):
+    return render(request,"Registro_Peleas.html")
+def registrarCaballero(request):
+    return render(request,"Registro_Caballeros.html")
 def registrarPareja(request):
     return render(request,"Registro_Parejas.html")
+    
 
  
